82a059c07120e9b95f797127edafbb2ebb51e870
Aleksi Vuorela <aleksi.vuorela@meiko.fi>
2025-10-21 13:15:30 +0300

    Export RenderOverrides type

--- packages/core/types/Config.tsx
+++ packages/core/types/Config.tsx
@@ -5,7 +5,7 @@ import { ComponentData, Metadata, RootData } from "./Data";
 import { AsFieldProps, WithChildren, WithId, WithPuckProps } from "./Utils";
 import { AppState } from "./AppState";
 import { DefaultComponentProps, DefaultRootFieldProps } from "./Props";
-import { Permissions } from "./API";
+import { Permissions, RenderOverrides } from "./API";
 import { DropZoneProps } from "../components/DropZone/types";
 import {
   ComponentConfigParams,
@@ -172,13 +172,7 @@ type ConfigInternal<
     >;
   };
   root?: RootConfigInternal<RootProps, UserField>;
-  overrides?: {
-    div?: RenderFunc<{
-      children?: ReactNode;
-      className?: string;
-      style?: React.CSSProperties;
-    }>;
-  };
+  overrides?: RenderOverrides;
 };
 
 type RenderFunc<

e1aa33fafb452778c1554cca0eaffaaca51e5085
Chris Villa <chrisvilla@me.com>
2025-12-03 16:59:45 +0000

    chore: remove unused SuperscriptControl from docs

--- apps/docs/components/Richtext/selector.ts
+++
@@ -1,8 +0,0 @@
-export const superscriptSelector = (ctx) => {
-  const editor = ctx.editor;
-  if (!editor) return {};
-  return {
-    isSuperscript: editor.isActive("superscript"),
-    canSuperscript: editor.can().chain().toggleSuperscript().run(),
-  };
-};

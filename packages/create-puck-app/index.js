abfa3c3972ebd1487e6495cbf40614bb9f1503a7
Federico Jorge Bonel Tozzi <bonelfederico@gmail.com>
2026-01-12 13:26:25 +0900

    fix: handle exits gracefully from create-puck-app

--- packages/create-puck-app/index.js
+++ packages/create-puck-app/index.js
@@ -38,6 +38,11 @@ function getPkgManager() {
   return "npm";
 }
 
+const handleSigTerm = () => process.exit(0);
+
+process.on("SIGINT", handleSigTerm);
+process.on("SIGTERM", handleSigTerm);
+
 program
   .command("create [app-name]")
   .option(

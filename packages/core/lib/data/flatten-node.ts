0289e7872e758931a9fa40bcbe758b0fe87d544d
Chris Villa <chrisvilla@me.com>
2026-01-12 19:35:43 +0000

    refactor: encode empty objects as strings to prevent stripping them
    a4bfae4f202ddaab12294cfffd2664d809e7cb54 stripped empty objects to prevent infinite renders in certain scenarios, but accidentally removed empty objects/arrays from expected scenarios. Instead of stripping them, this approach encodes/decodes the empty objects as strings, solving the original re-rendering problem without losing the value.

--- packages/core/lib/data/flatten-node.ts
+++ packages/core/lib/data/flatten-node.ts
@@ -5,31 +5,47 @@ import { stripSlots } from "./strip-slots";
 // Explicitly destructure to account for flat module issues: https://github.com/puckeditor/puck/issues/1089
 const { flatten, unflatten } = flat;
 
-function isEmptyArrayOrObject(val: any): boolean {
-  if (Array.isArray(val)) {
-    return val.length === 0;
-  }
+const isPureObject = (val: any) =>
+  val != null && Object.prototype.toString.call(val) === "[object Object]";
 
-  if (
-    val != null &&
-    Object.prototype.toString.call(val) === "[object Object]"
-  ) {
-    return Object.keys(val).length === 0;
-  }
+const emptyArrayStr = "__puck_[]";
+const emptyObjectStr = "__puck_{}";
 
-  return false;
-}
-
-function stripEmptyObjects(props: Record<string, any>) {
+function encodeEmptyObjects(props: Record<string, any> = {}) {
   const result: Record<string, any> = {};
 
   for (const key in props) {
     if (!Object.prototype.hasOwnProperty.call(props, key)) continue;
 
     const val = props[key];
-    if (isEmptyArrayOrObject(val)) continue;
 
-    result[key] = val;
+    if (Array.isArray(val)) {
+      result[key] = emptyArrayStr;
+    } else if (isPureObject(val) && Object.keys(val).length === 0) {
+      result[key] = emptyObjectStr;
+    } else {
+      result[key] = val;
+    }
+  }
+
+  return result;
+}
+
+function decodeEmptyObjects(props: Record<string, any> = {}) {
+  const result: Record<string, any> = {};
+
+  for (const key in props) {
+    if (!Object.prototype.hasOwnProperty.call(props, key)) continue;
+
+    const val = props[key];
+
+    if (val === emptyArrayStr) {
+      result[key] = [];
+    } else if (val === emptyObjectStr) {
+      result[key] = {};
+    } else {
+      result[key] = val;
+    }
   }
 
   return result;
@@ -44,12 +60,12 @@ export const flattenNode = <
 ) => {
   return {
     ...node,
-    props: stripEmptyObjects(flatten(stripSlots(node, config).props)),
+    props: encodeEmptyObjects(flatten(stripSlots(node, config).props)),
   };
 };
 
 export const expandNode = (node: ComponentData | RootData) => {
-  const props = unflatten(node.props);
+  const props = unflatten(decodeEmptyObjects(node.props));
 
   return {
     ...node,
